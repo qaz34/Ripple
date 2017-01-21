a204731b1393632a6ed5517d969e85481ed0b750
Cameron <cameron.oborn@gmail.com>
2017-01-21 10:30:34 +1030

    no message

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharController : MonoBehaviour {
+public class CharController : MonoBehaviour
+{
+    public float speed = 10;
+    Rigidbody2D m_rb;
+    Vector3 move;
+    // Use this for initialization
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        move *= Time.deltaTime * speed;
+        move = transform.position + move;
+
+    }
+    void FixedUpdate()
+    {
+        move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        m_rb.velocity = move * Time.fixedDeltaTime * speed;
+    }
 }
